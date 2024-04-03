3ffba1fb36cc0e1e1bf5fe93a4f09a7ccc10fd37
NajwaJauhari <12250124838@students.uin-suska.ac.id>
2024-04-03 15:05:25 +0700

    done.

--- app/src/main/java/com/example/tugasday06/DetailActivity.java
+++ app/src/main/java/com/example/tugasday06/DetailActivity.java
@@ -1,77 +1,93 @@
 package com.example.tugasday06;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
+    private String name, price, specific;
+    private int image;
     private ImageView imgDetail;
-    private TextView txtDetail;
-    private Button btnShare;
-    public static final String KEY_DATA = "key_data";
+    private TextView txtDetailName, txtDetailPrice, txtDetailSpecific;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         imgDetail = findViewById(R.id.imgDetail);
-        txtDetail = findViewById(R.id.txtDetail);
-        btnShare = findViewById(R.id.btnShare);
+        txtDetailName = findViewById(R.id.txtDetailName);
+        txtDetailPrice = findViewById(R.id.txtDetailPrice);
+        txtDetailSpecific = findViewById(R.id.txtDetailSpecific);
 
-        Detail detail = getIntent().getParcelableExtra(KEY_DATA);
+        name = getIntent().getStringExtra("name");
+        price = getIntent().getStringExtra("price");
+        specific = getIntent().getStringExtra("specific");
+        image = getIntent().getIntExtra("image", 0);
 
-        String sourceImage = detail.getImage();
-        int sourceImageID = getResources().getIdentifier(sourceImage, "drawable", getPackageName());
-        imgDetail.setImageResource(sourceImageID);
+        txtDetailName.setText(name);
+        txtDetailPrice.setText(price);
+        txtDetailSpecific.setText(specific);
+        imgDetail.setImageResource(image);
 
-        String specific = detail.getSpecific();
-        String price = detail.getPrice();
-
-        String gabungan = specific + price;
-
-        txtDetail.setText(gabungan);
+        Button btnShare = findViewById(R.id.btnShare);
         btnShare.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btnShare) {
-            shareDetailAndImage();
+            shareItem();
         }
     }
 
-    private void shareDetailAndImage() {
-        BitmapDrawable bitmapDrawable = (BitmapDrawable) imgDetail.getDrawable();
-        Bitmap bitmap = bitmapDrawable.getBitmap();
+    private void shareItem() {
+        BitmapDrawable drawable = (BitmapDrawable) imgDetail.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
 
-        Uri uri = saveImage(bitmap);
+        try {
+            Context context = getApplicationContext();
+            File cachePath = new File(context.getCacheDir(), "images");
+            cachePath.mkdirs();
+            FileOutputStream stream = new FileOutputStream(cachePath + "/image.png"); // overwrites this image every time
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        String detailText = txtDetail.getText().toString();
-        String shareText = detailText;
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.setType("image/*");
-        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
-        shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+        File imagePath = new File(getApplicationContext().getCacheDir(), "images");
+        File newFile = new File(imagePath, "image.png");
+        Uri contentUri = FileProvider.getUriForFile(getApplicationContext(), "com.example.tugasday6.fileprovider", newFile);
 
-        startActivity(Intent.createChooser(shareIntent, "Share Detail via"));
-    }
+        if (contentUri != null) {
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            shareIntent.setDataAndType(contentUri, getContentResolver().getType(contentUri));
 
-    private Uri saveImage(Bitmap bitmap) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "DetailImage", null);
-        return Uri.parse(path);
+            String shareText = "Check out this product: " + name + "\nPrice: " + price + "\nDescription: " + specific;
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+
+            shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+            shareIntent.setType("image/png");
+            startActivity(Intent.createChooser(shareIntent, "Choose an app"));
+        }
     }
 }
--- app/src/main/java/com/example/tugasday06/MainActivity.java
+++ app/src/main/java/com/example/tugasday06/MainActivity.java
@@ -1,47 +1,50 @@
 package com.example.tugasday06;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FashionAdapter fashionAdapter;
+    private ArrayList<com.example.tugasday6.FashionModel> fashionModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView ivFashion = findViewById(R.id.ivFashion);
-        ImageView ivPlushies = findViewById(R.id.ivPlushies);
-        ImageView ivMakeUp = findViewById(R.id.ivMakeUp);
-        Button btnFashion = findViewById(R.id.btnFashion);
-        Button btnPlushies = findViewById(R.id.btnPlushies);
-        Button btnMakeUp = findViewById(R.id.btnMakeUp);
+        getData();
 
-        ivFashion.setOnClickListener(this);
-        ivPlushies.setOnClickListener(this);
-        ivMakeUp.setOnClickListener(this);
-        btnFashion.setOnClickListener(this);
-        btnPlushies.setOnClickListener(this);
-        btnMakeUp.setOnClickListener(this);
+        recyclerView = findViewById(R.id.recycleView);
+        fashionAdapter = new FashionAdapter(fashionModel, this);
 
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(fashionAdapter);
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.ivFashion || v.getId() == R.id.btnFashion){
-            Intent intent = new Intent(this, FashionActivity.class);
-            startActivity(intent);
-        } else if (v.getId() == R.id.ivPlushies || v.getId() == R.id.btnPlushies) {
-            Intent intent = new Intent(this, PlushiesActivity.class);
-            startActivity(intent);
-        } else if (v.getId() == R.id.ivMakeUp || v.getId() == R.id.btnMakeUp) {
-            Intent intent = new Intent(this, MakeUpActivity.class);
-            startActivity(intent);
-        }
+    private void getData(){
+        fashionModel = new ArrayList<>();
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_kf1), getString(R.string.price_kf1),getString(R.string.kf1), R.drawable.kf1));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_kf2), getString(R.string.price_kf2), getString(R.string.kf2),R.drawable.kf2));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_kf3), getString(R.string.price_kf3), getString(R.string.kf3),R.drawable.kf3));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_kf4), getString(R.string.price_kf4), getString(R.string.kf4),R.drawable.kf4));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_kf5), getString(R.string.price_kf5), getString(R.string.kf5),R.drawable.kf5));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_mk1), getString(R.string.price_mk1), getString(R.string.mk1),R.drawable.mk1));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_mk2), getString(R.string.price_mk2), getString(R.string.mk2),R.drawable.mk2));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_mk3), getString(R.string.price_mk3), getString(R.string.mk3),R.drawable.mk3));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_mk4), getString(R.string.price_mk4), getString(R.string.mk4),R.drawable.mk4));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_mk5), getString(R.string.price_mk5), getString(R.string.mk5),R.drawable.mk5));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_p1), getString(R.string.price_p1), getString(R.string.p1),R.drawable.p1));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_p2), getString(R.string.price_p2), getString(R.string.p2),R.drawable.p2));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_p3), getString(R.string.price_p3), getString(R.string.p3),R.drawable.p3));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_p4), getString(R.string.price_p4), getString(R.string.p4),R.drawable.p4));
+        fashionModel.add(new com.example.tugasday6.FashionModel(getString(R.string.name_p5), getString(R.string.price_p5), getString(R.string.p5),R.drawable.p5));
     }
 }
